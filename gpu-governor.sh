7f693994ddd15d1a15f7148369ee0021eff5d9f5
SnipeX_ <andrej.halv@gmail.com>
2022-04-23 20:30:56 +0200

    remove unnecessary sudo commands

--- gpu-governor.sh
+++ gpu-governor.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo sh -c 'echo "auto" > /sys/class/drm/card1/device/power_dpm_force_performance_level'
+echo "auto" > /sys/class/drm/card1/device/power_dpm_force_performance_level
 sleep 1
 
 while :
 do
 	while [[ $(cat /sys/class/drm/card0/device/power/runtime_status) == active ]]
 	do
-		sudo sh -c 'echo "auto" > /sys/class/drm/card1/device/power_dpm_force_performance_level'
+		echo "auto" > /sys/class/drm/card1/device/power_dpm_force_performance_level
 		sleep 1
 	done
 
@@ -14,11 +14,11 @@ do
 		then
 			while [ $(cat /sys/class/drm/card1/device/gpu_busy_percent) -gt 34 ]
 			do
-				sudo sh -c 'echo "high" > /sys/class/drm/card1/device/power_dpm_force_performance_level'
+				echo "high" > /sys/class/drm/card1/device/power_dpm_force_performance_level
 				sleep 1
 			done
 		else
-			sudo sh -c 'echo "low" > /sys/class/drm/card1/device/power_dpm_force_performance_level'
+			echo "low" > /sys/class/drm/card1/device/power_dpm_force_performance_level
 		fi
 sleep 1
 done
